468411a9c07587dc26d7b34fb0b410c18170bddc
Tobias Kremer <t.kremer@reply.de>
2020-05-07 08:47:02 +0200

    create new Component UserInput

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class UserInput extends Component {
+
+    render = () => {
+        return(
+            <div className="UserInput">
+                <p>first para</p>
+                <p>second para</p>
+            </div>
+        );
+    }
+}
+
+export default UserInput;
