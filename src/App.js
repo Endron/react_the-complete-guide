50ade3ba481b08d5fa608bc6b0c71a18d43dec1c
Tobias Kremer <t.kremer@reply.de>
2020-05-07 08:50:51 +0200

    add new Component UserOutput

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 function App() {
   return (
     <div className="App">
       <UserInput />
-      <UserInput />
-      <UserInput />
-      <UserInput />
+      <UserOutput />
+      <UserOutput />
+      <UserOutput />
     </div>
   );
 }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class UserOutput extends Component {
+
+    render = () => {
+        return (
+            <div>
+                <p>first</p>
+                <p>second</p>
+            </div>
+        );
+    }
+}
+
+export default UserOutput;
