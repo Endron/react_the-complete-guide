a79b2596e7a894d756fab65d0dcc2e9295e07ca9
Endron <Endron@users.noreply.github.com>
2020-05-07 07:57:42 +0000

    deploy: ebda40ba346c3a3fa6909c7ea68d7af890ae38fb

---
+++ precache-manifest.accd29d671975eefef3df7ebcc8a2d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908251190edf99d970f8a9a8a2b71d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687a2170ed6f3115d01f",
+    "url": "/static/css/main.ec352870.chunk.css"
+  },
+  {
+    "revision": "e711b24145c35867b073",
+    "url": "/static/js/2.b8b2dfe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8b2dfe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687a2170ed6f3115d01f",
+    "url": "/static/js/main.32e61df8.chunk.js"
+  },
+  {
+    "revision": "afccb5c9973f4a009171",
+    "url": "/static/js/runtime-main.5a05de8d.js"
+  }
+]);
